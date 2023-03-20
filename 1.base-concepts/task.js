57ee4281043aac97ebfad67a9e2091b731c9d7f6
Saylance2503 <yurik_05@inbox.ru>
2023-03-21 00:47:23 +0400

    fit: base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,36 @@
-"use strict"
+'use strict';
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    arr = [];
+  }
+  if (d === 0) {
+    let x1 = -b / (2 * a);
+    arr = [x1];
+  }
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr = [x1, x2];
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let body;
+  let payment;
+  let paymentAll;
+
+  if (typeof percent !== 'number' || typeof contribution !== 'number' || typeof amount !== 'number') {
+    return false;
+  }
+  percent = percent / 100 / 12;
+  body = amount - contribution;
+
+  payment = body * (percent + percent / ((1 + percent) ** countMonths - 1));
+  paymentAll = payment * countMonths;
+  return +paymentAll.toFixed(2);
 }
